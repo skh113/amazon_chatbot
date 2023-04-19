8793789273ab8c7a989876f8c44e640600015b50
Keyvan Hosseini <skhprogramming@gmail.com>
2023-04-19 20:26:07 +0330

    Edited the Question model

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question = models.TextField(null=False, blank=False, default="")
-    created_on = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Question was: {self.question}"
-
-
 class Answer(models.Model):
     answer = models.TextField(default="")
-    question_number = models.OneToOneField(Question, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Answer was: {self.answer}"
+
+
+class Question(models.Model):
+    question = models.TextField(null=False, blank=False, default="")
+    answer_number = models.OneToOneField(Answer, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Question was: {self.question}"
